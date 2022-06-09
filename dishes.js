4b878ce7f389317f148f98d37b1568cee785c43f
Phil Schmucker <p.schmuck.er@web.de>
2022-06-09 17:39:54 +0200

    styling navbar and header

---
+++ dishes.js
@@ -0,0 +1,8 @@
+let soups = [
+  {
+    names: "['Misosuppe','Lachssuppe']",
+    infos:
+      "['typisch gewürzt, verfeinert mit Tofu, Seetang und Frühlingszwiebeln','mit Algen, Frühlingszwiebeln und Lachsfilet-Stücken']",
+    prices: "['3.6','4.6']",
+  },
+];
