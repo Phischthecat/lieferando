1ff33775a1b81b3fdc3baba47582e12b330deae6
Phil Schmucker <p.schmuck.er@web.de>
2022-06-10 16:30:55 +0200

    js render + styling

--- script.js
+++ script.js
@@ -17,4 +17,59 @@ function updateBasket() {
   document.getElementById('finalSum').innerHTML = finalSum;
 }
 
-function render() {}
+function render() {
+  let food = document.getElementById('food');
+  food.innerHTML = '';
+
+  for (let i = 0; i < dishes.length; i++) {
+    const subheader = dishes[i];
+    food.innerHTML += /*html*/ `
+    <div class="container-dishes">
+    <div class="header-sections">
+      <h2 class="subheadline">${subheader['type']}</h2>
+      <span class="extraInfo">${subheader['extraInfo']}</span>
+    </div>
+    <div id="dishes-sections${i}"></div>    
+    </div>`;
+
+    let dishContent = document.getElementById(`dishes-sections${i}`);
+
+    for (let j = 0; j < subheader['names'].length; j++) {
+      const names = subheader['names'][j];
+      const infos = subheader['infos'][j];
+      const prices = subheader['prices'][j];
+
+      dishContent.innerHTML += /*html*/ `
+      <div class="dish-box">
+      <div>
+        <h3 class="dish-name">${names}</h3>
+        <p class="dish-info">${infos}</p>
+      </div>  
+        <p class="dish-price">${prices.replace('.', ',')}0 €</p>
+        <div class="plusSign" onclick="addToBasket()">
+        <a href="#"><img src="./img/icons/plus_blue.png" alt=""></a>  
+        </div>
+      </div>
+      `;
+    }
+  }
+}
+
+function generateSubheader(i) {
+  return /*html*/ `
+  <div class="header-sections">
+    <h2 class="subheadline">${subheader['type']}</h2>
+    <span class="extraInfo">${subheader['extraInfo']}</span>
+  </div>
+  <div id="dishes-sections${i}"></div>
+  <!-- <button onclick="addToBasket()">+</button> -->
+  `;
+}
+
+function generateFoodBox(j) {
+  return /*html*/ `
+<div class="dish-box">
+  <h3>${names}</h3>
+</div>
+`;
+}
